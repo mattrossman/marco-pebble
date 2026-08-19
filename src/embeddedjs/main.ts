95c8e93bf05aa7da2a0764e9dee4843e749d19d5
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2026-08-19 19:18:23 -0400

    feat: add static speaker status UI

--- src/embeddedjs/main.ts
+++ src/embeddedjs/main.ts
@@ -1,7 +1,60 @@
+import {} from "piu/MC";
 import Button from "pebble/button";
 
 let playing = false;
 let restartTimer: any;
+
+const backgroundSkin = new Skin({ fill: "black" });
+const titleStyle = new Style({
+	font: "bold 18px Gothic",
+	color: "white",
+	horizontal: "center",
+	vertical: "middle"
+});
+const statusStyle = new Style({
+	font: "bold 28px Gothic",
+	color: "white",
+	horizontal: "center",
+	vertical: "middle"
+});
+const hintStyle = new Style({
+	font: "14px Gothic",
+	color: "white",
+	horizontal: "center",
+	vertical: "middle"
+});
+
+const title = new Label(null, {
+	top: 28,
+	left: 0,
+	right: 0,
+	height: 30,
+	string: "MARCO PEBBLE",
+	style: titleStyle
+});
+const status = new Label(null, {
+	top: 78,
+	left: 0,
+	right: 0,
+	height: 42,
+	string: "READY",
+	style: statusStyle
+});
+const hint = new Label(null, {
+	bottom: 28,
+	left: 0,
+	right: 0,
+	height: 24,
+	string: "SELECT: START SOUND",
+	style: hintStyle
+});
+
+const application = new Application(null, {
+	skin: backgroundSkin
+});
+application.add(title);
+application.add(status);
+application.add(hint);
 
 declare const Natives: {
 	marco_speaker_play(): number;
@@ -14,6 +67,8 @@ function startTone() {
 
 	playing = Natives.marco_speaker_play() !== 0;
 	console.log(playing ? "Speaker tone on" : "Speaker tone failed to start");
+	status.string = playing ? "PINGING" : "READY";
+	hint.string = playing ? "SELECT: STOP SOUND" : "SELECT: START SOUND";
 
 	if (playing)
 		restartTimer = setInterval(() => Natives.marco_speaker_play(), 1500);
@@ -30,6 +85,8 @@ function stopTone() {
 
 	Natives.marco_speaker_stop();
 	playing = false;
+	status.string = "READY";
+	hint.string = "SELECT: START SOUND";
 	console.log("Speaker tone off");
 }
 
